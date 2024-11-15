8a3584a1e64a4d8b04e41c7dece0fb19f66a2101
Luciano Wayand de Abreu <36968613+lucianowayand@users.noreply.github.com>
2024-11-15 20:04:05 -0300

    Initial Commit

---
+++ app/src/main/java/io/github/lucianoawayand/lubank_app/MainActivity.java
@@ -0,0 +1,48 @@
+package io.github.lucianoawayand.lubank_app;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import io.github.lucianoawayand.lubank_app.utils.MaskEditUtil;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText govRegCodeInput;
+    EditText passwordInput;
+    Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        govRegCodeInput = findViewById(R.id.govRegCode_input);
+        govRegCodeInput.addTextChangedListener(MaskEditUtil.mask(govRegCodeInput, MaskEditUtil.FORMAT_CPF));
+
+        passwordInput = findViewById(R.id.password_input);
+        loginButton = findViewById(R.id.login_btn);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("LUBANK", govRegCodeInput.getText().toString());
+            }
+        });
+    }
+}
