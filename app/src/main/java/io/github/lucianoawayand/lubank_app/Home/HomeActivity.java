b5eb30593e8728e4edf44c84efe682e4dd0c8cdf
luigiboscatto <luigibboscatto@gmail.com>
2024-11-17 21:15:49 -0300

    fixed actions arraylist

--- app/src/main/java/io/github/lucianoawayand/lubank_app/Home/HomeActivity.java
+++ app/src/main/java/io/github/lucianoawayand/lubank_app/Home/HomeActivity.java
@@ -9,39 +9,33 @@ import java.util.ArrayList;
 
 import io.github.lucianoawayand.lubank_app.R;
 import io.github.lucianoawayand.lubank_app.adapter.ActionsAdapter;
-import io.github.lucianoawayand.lubank_app.databinding.ActivityHomeBinding;
 import io.github.lucianoawayand.lubank_app.model.Action;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private ActivityHomeBinding binding;
-    private ArrayList<Action> actionsList;
-    private ActionsAdapter actionsAdapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_home);
 
         RecyclerView recyclerView = findViewById(R.id.recyclerViewActions);
+
+        ArrayList<Action> actions = createActionsList();
+
         LinearLayoutManager layoutManager
                 = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
-        actionsAdapter = new ActionsAdapter(this, actionsList);
+
+        ActionsAdapter actionsAdapter = new ActionsAdapter(this, actions);
         recyclerView.setAdapter(actionsAdapter);
-        createActionsList();
     }
 
-    private void createActionsList() {
-        Action action1 = new Action(R.drawable.ic_dollasign, "Fazer transferência");
-        actionsList.add(action1);
+    private ArrayList<Action> createActionsList() {
+        ArrayList<Action> actions = new ArrayList<>();
 
-        Action action2 = new Action(R.drawable.ic_dollasign, "Fazer transferência 1123");
-        actionsList.add(action1);
+        actions.add(new Action(R.drawable.ic_dollasign, "Fazer transferência"));
 
-        Action action3 = new Action(R.drawable.ic_dollasign, "Fazer transferência asdasdas");
-        actionsList.add(action1);
+        return actions;
     }
 }
