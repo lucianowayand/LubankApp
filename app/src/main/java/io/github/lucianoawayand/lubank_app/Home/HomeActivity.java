e0f1d438a40b95a8c7f2a73fb1ce65059a941e90
luigiboscatto <luigibboscatto@gmail.com>
2024-11-20 18:44:25 -0300

    exit button

--- app/src/main/java/io/github/lucianoawayand/lubank_app/Home/HomeActivity.java
+++ app/src/main/java/io/github/lucianoawayand/lubank_app/Home/HomeActivity.java
@@ -3,12 +3,17 @@ package io.github.lucianoawayand.lubank_app.Home;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -19,6 +24,7 @@ import com.google.gson.Gson;
 
 import io.github.lucianoawayand.lubank_app.Deposits.DepositsActivity;
 import io.github.lucianoawayand.lubank_app.Home.model.Action;
+import io.github.lucianoawayand.lubank_app.Main.MainActivity;
 import io.github.lucianoawayand.lubank_app.R;
 import io.github.lucianoawayand.lubank_app.Home.adapter.ActionsAdapter;
 import io.github.lucianoawayand.lubank_app.Transactions.SendTransactionActivity;
@@ -47,6 +53,9 @@ public class HomeActivity extends AppCompatActivity {
 
         transactionService = RetrofitClient.getClient(this).create(TransactionService.class);
 
+        ImageView icExit = findViewById(R.id.ic_exit);
+        icExit.setOnClickListener(v -> showLogoutConfirmationDialog());
+
         Gson gson = new Gson();
         SharedPreferences sharedPreferences = getSharedPreferences("user_prefs", Context.MODE_PRIVATE);
 
@@ -62,7 +71,6 @@ public class HomeActivity extends AppCompatActivity {
 
         txtAccountBalance = findViewById(R.id.txtAccountBalance);
         swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout);
-
 
         // Setup RecyclerView for Action cards
         RecyclerView recyclerView = findViewById(R.id.recyclerViewActions);
@@ -129,5 +137,41 @@ public class HomeActivity extends AppCompatActivity {
                 Log.d("GetBalance", "Error getting balance");
             }
         });
+    }
+
+    private void showLogoutConfirmationDialog() {
+        View dialogView = getLayoutInflater().inflate(R.layout.custom_dialog, null);
+
+        final AlertDialog dialog = new AlertDialog.Builder(this)
+                .setView(dialogView)
+                .setCancelable(true)
+                .create();
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        Button btnConfirm = dialogView.findViewById(R.id.btnConfirm);
+        Button btnCancel = dialogView.findViewById(R.id.btnCancel);
+
+        btnConfirm.setOnClickListener(v -> {
+            logoutUser();
+            dialog.dismiss();
+        });
+
+        btnCancel.setOnClickListener(v -> dialog.dismiss());
+
+        dialog.show();
+    }
+
+
+    private void logoutUser() {
+        SharedPreferences sharedPreferences = getSharedPreferences("user_prefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.remove("jwt-token");
+        editor.remove("user");
+        editor.apply();
+
+        Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // Clear activity stack
+        startActivity(intent);
+        finish();
     }
 }
