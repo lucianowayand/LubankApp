ebe2b417d0e40ad4258439cb6476db4c8f76adce
Luciano Wayand de Abreu <36968613+lucianowayand@users.noreply.github.com>
2024-11-20 17:18:20 -0300

    feat: trasactions form

--- app/src/main/java/io/github/lucianoawayand/lubank_app/Transactions/SendTransactionActivity.java
+++ app/src/main/java/io/github/lucianoawayand/lubank_app/Transactions/SendTransactionActivity.java
@@ -2,27 +2,40 @@ package io.github.lucianoawayand.lubank_app.Transactions;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import io.github.lucianoawayand.lubank_app.Home.HomeActivity;
-import io.github.lucianoawayand.lubank_app.Main.MainActivity;
 import io.github.lucianoawayand.lubank_app.R;
-import io.github.lucianoawayand.lubank_app.Register.RegisterActivity;
-import io.github.lucianoawayand.lubank_app.Register.domain.CreateUserRequestDto;
+import io.github.lucianoawayand.lubank_app.shared.config.RetrofitClient;
+import io.github.lucianoawayand.lubank_app.shared.services.TransactionService;
+import io.github.lucianoawayand.lubank_app.shared.utils.MaskEditUtil;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SendTransactionActivity extends AppCompatActivity {
     private Button backButton;
+    private EditText ammountInput;
+    private TextView balanceAmmount;
+    private Double accountBalance;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initialSetup();
+
+        TransactionService transactionService = RetrofitClient.getClient(this).create(TransactionService.class);
+        setAccountBalance(transactionService);
 
         backButton = findViewById(R.id.return_button);
         backButton.setOnClickListener(new View.OnClickListener() {
@@ -31,6 +44,36 @@ public class SendTransactionActivity extends AppCompatActivity {
                 Intent intent = new Intent(SendTransactionActivity.this, HomeActivity.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+
+        ammountInput = findViewById(R.id.ammount_input);
+        ammountInput.addTextChangedListener(MaskEditUtil.mask(ammountInput, MaskEditUtil.FORMAT_MONEY));
+
+
+    }
+
+    private void setAccountBalance(TransactionService transactionService) {
+        Call<Double> call = transactionService.getBalance();
+        call.enqueue(new Callback<Double>() {
+            @Override
+            public void onResponse(@NonNull Call<Double> call, @NonNull Response<Double> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    accountBalance = response.body();
+                    String moneyAmmount = (String) MaskEditUtil.formatMoney(accountBalance);
+
+                    Log.e("LUBANK", accountBalance.toString() + " " + moneyAmmount);
+                    balanceAmmount = findViewById(R.id.balance_ammount);
+                    balanceAmmount.setText(moneyAmmount);
+                } else {
+                    Log.d("GetBalance", "Error getting balance");
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Double> call, Throwable t) {
+                Log.d("GetBalance", "Error getting balance");
             }
         });
     }
--- app/src/main/java/io/github/lucianoawayand/lubank_app/shared/utils/MaskEditUtil.java
+++ app/src/main/java/io/github/lucianoawayand/lubank_app/shared/utils/MaskEditUtil.java
@@ -11,12 +11,13 @@ public class MaskEditUtil {
     public static final String FORMAT_CEP = "#####-###";
     public static final String FORMAT_DATE = "##/##/####";
     public static final String FORMAT_HOUR = "##:##";
+    public static final String FORMAT_MONEY = "R$ #,##0.00"; // Currency format
 
     /**
      * Método que deve ser chamado para realizar a formatação
      *
-     * @param ediTxt
-     * @param mask
+     * @param ediTxt EditText to format
+     * @param mask   Mask to apply
      * @return TextWatcher for applying the mask
      */
     public static TextWatcher mask(final EditText ediTxt, final String mask) {
@@ -25,49 +26,50 @@ public class MaskEditUtil {
             String previousText = "";
 
             @Override
-            public void afterTextChanged(final Editable s) {}
+            public void afterTextChanged(final Editable s) {
+                // No action needed here
+            }
 
             @Override
-            public void beforeTextChanged(final CharSequence s, final int start, final int count, final int after) {}
+            public void beforeTextChanged(final CharSequence s, final int start, final int count, final int after) {
+                // Save the previous state in case of user edits
+                previousText = s.toString();
+            }
 
             @Override
             public void onTextChanged(final CharSequence s, final int start, final int before, final int count) {
-                // Avoid recursion if we are already updating
                 if (isUpdating) return;
 
                 String unmaskedText = unmask(s.toString());
-                String formattedText = "";
-                int i = 0;
+                String formattedText;
 
-                // Dynamically switch mask if CPF is longer than 11 characters
-                String currentMask = mask;
-                if (unmaskedText.length() > 11 && mask.equals(FORMAT_CPF)) {
-                    currentMask = FORMAT_CNPJ;
-                }
+                try {
+                    isUpdating = true;
 
-                // Format based on the selected mask
-                for (char m : currentMask.toCharArray()) {
-                    if (m != '#' && unmaskedText.length() > i) {
-                        formattedText += m;
+                    if (mask.equals(FORMAT_MONEY)) {
+                        // Parse unmasked text as a long to avoid floating-point issues
+                        long value = unmaskedText.isEmpty() ? 0 : Long.parseLong(unmaskedText);
+                        formattedText = (String) formatMoney(value / 100.0); // Convert to "real" amount
                     } else {
-                        try {
-                            formattedText += unmaskedText.charAt(i);
-                        } catch (Exception e) {
-                            break;
-                        }
-                        i++;
+                        // Apply other masks (CPF, CNPJ, etc.)
+                        formattedText = applyMask(unmaskedText, mask);
                     }
-                }
 
-                // Prevent infinite recursion
-                isUpdating = true;
-                ediTxt.setText(formattedText);
-                ediTxt.setSelection(formattedText.length());
-                isUpdating = false;
+                    // Update the EditText
+                    ediTxt.setText(formattedText);
+                    ediTxt.setSelection(formattedText.length());
+                } catch (Exception e) {
+                    // Restore the previous state if an error occurs
+                    ediTxt.setText(previousText);
+                    ediTxt.setSelection(previousText.length());
+                } finally {
+                    isUpdating = false;
+                }
             }
         };
     }
 
+    // Method to unmask the string (remove formatting characters)
     public static String unmask(final String s) {
         return s.replaceAll("[.]", "")
                 .replaceAll("[-]", "")
@@ -75,6 +77,36 @@ public class MaskEditUtil {
                 .replaceAll("[(]", "")
                 .replaceAll("[ ]", "")
                 .replaceAll("[:]", "")
-                .replaceAll("[)]", "");
+                .replaceAll("[)]", "")
+                .replaceAll("[R$]", "")
+                .replaceAll(",", "");
+    }
+
+    // Method to format the money string
+    public static CharSequence formatMoney(Double value) {
+        if (value == null) return "R$ 0,00";
+
+        java.text.DecimalFormat format = new java.text.DecimalFormat("R$ #,##0.00");
+        return format.format(value);
+    }
+
+    // Apply other types of masks (e.g., CPF, CNPJ)
+    private static String applyMask(String unmaskedText, String mask) {
+        String formattedText = "";
+        int i = 0;
+
+        for (char m : mask.toCharArray()) {
+            if (m != '#' && unmaskedText.length() > i) {
+                formattedText += m;
+            } else {
+                try {
+                    formattedText += unmaskedText.charAt(i);
+                } catch (Exception e) {
+                    break;
+                }
+                i++;
+            }
+        }
+        return formattedText;
     }
 }
