f53cad92c88c1fa9cb664db7ff3b29d0e443de08
Luciano Wayand de Abreu <36968613+lucianowayand@users.noreply.github.com>
2024-11-20 22:52:35 -0300

    fix: back button not working on list transactions

--- app/src/main/java/io/github/lucianoawayand/lubank_app/Transactions/ListTransactionsActivity.java
+++ app/src/main/java/io/github/lucianoawayand/lubank_app/Transactions/ListTransactionsActivity.java
@@ -2,13 +2,19 @@ package io.github.lucianoawayand.lubank_app.Transactions;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
+import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -21,6 +27,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.TimeZone;
 
+import io.github.lucianoawayand.lubank_app.Home.HomeActivity;
 import io.github.lucianoawayand.lubank_app.R;
 import io.github.lucianoawayand.lubank_app.Transactions.adapter.ListTransactionsAdapter;
 import io.github.lucianoawayand.lubank_app.shared.config.RetrofitClient;
@@ -32,7 +39,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class ListTransactionsActivity extends AppCompatActivity {
-
+    private Button backButton;
     private RecyclerView recyclerView;
     private View progressOverlay;
     private ListTransactionsAdapter adapter;
@@ -40,13 +47,27 @@ public class ListTransactionsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_extract);
+        initialSetup();
 
         recyclerView = findViewById(R.id.recyclerViewActions);
         progressOverlay = findViewById(R.id.progressOverlay);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         fetchTransactions();
+
+        backButton = findViewById(R.id.return_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                redirectToHome();
+            }
+        });
+    }
+
+    private void redirectToHome() {
+        Intent intent = new Intent(ListTransactionsActivity.this, HomeActivity.class);
+        startActivity(intent);
+        finish();
     }
 
     private void fetchTransactions() {
@@ -101,5 +122,15 @@ public class ListTransactionsActivity extends AppCompatActivity {
 
     private void showLoading(boolean show) {
         progressOverlay.setVisibility(show ? View.VISIBLE : View.GONE);
+    }
+
+    private void initialSetup() {
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_extract);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
